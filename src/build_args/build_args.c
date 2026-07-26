598ff611098c91cb0e586bbd24a6da3581ffd27e
mat <61627473+mvle29@users.noreply.github.com>
2026-07-26 04:42:24 +0200

    fix: added a remove quotes function/step for redirections. now ls>"test" wont create a file litterally named "test" but just test. Ambiguous redirect are kept when necessary

--- src/build_args/build_args.c
+++ src/build_args/build_args.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 01:33:10 by mat               #+#    #+#             */
-/*   Updated: 2026/07/01 01:29:02 by marvin           ###   ########.fr       */
+/*   Updated: 2026/07/26 04:39:58 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,32 @@ int	ambiguous_redirect(t_redirs **redirs)
 	return (0);
 }
 
+int	remove_quotes_redirs(t_redirs **redirs)
+{
+	t_mode	mode;
+	int		i;
+	int		j;
+
+	j = -1;
+	while (redirs[++j])
+	{
+		if (redirs[j]->file == NULL)
+			continue ;
+		mode = NORMAL;
+		i = 0;
+		while (redirs[j]->file->lexeme[i])
+		{
+			if (mode_change(&mode, redirs[j]->file->lexeme, i))
+				ft_memmove(redirs[j]->file->lexeme + i,
+					redirs[j]->file->lexeme + i + 1,
+					1 + ft_strlen(redirs[j]->file->lexeme + i));
+			else
+				i++;
+		}
+	}
+	return (1);
+}
+
 int	build_args_recursive(t_ast *ast, int last_status, char **envp)
 {
 	t_list	*args_lst;
@@ -51,6 +77,7 @@ int	build_args_recursive(t_ast *ast, int last_status, char **envp)
 	{
 		if (!expand(ast, last_status, envp) || ambiguous_redirect(ast->redirs))
 			return (0);
+		remove_quotes_redirs(ast->redirs);
 		args_lst = NULL;
 		if (!tokens_to_lst(&args_lst, ast->start, ast->end))
 			return (0);
